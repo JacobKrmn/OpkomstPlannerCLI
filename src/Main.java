ed98a9ace71a0c9ca78e5fe34f188c32c935ffbc
JacobKrmn <jacob4karman@gmail.com>
2022-05-23 17:43:38 +0200

    Test commit 2 (version change)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    private static final String VERSION = "1.0.0";
+    private static final String VERSION = "1.1.0";
 
     public static void main(String[] args) {
         Planner planner = new Planner();
